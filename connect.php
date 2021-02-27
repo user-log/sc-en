82ba9bd6435a681880f678980f94d98fad6f9e10
user-log <76106375+user-log@users.noreply.github.com>
2021-02-27 19:56:40 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,7 +6,7 @@ $password = "012166Root7980";
 
 
 try {
-    $con = new PDO("mysql:host=freedb.tech;dbname=freedbtech_bassemowo63", $username, $password);
+    $con = new PDO("mysql:host=freedb.tech;dbname=freedbtech_bassemowo", $username, $password);
     
     $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
